cdc9872c62901e3c7e4c11bf85b3985c6e9c5e75
Anthony House <tuffant21@aim.com>
2018-11-24 14:30:54 -0600

    Did changes requested by Steve. Added a new paramenter in mutate.c and mutate.h for int *round. Passed those values in from input.c.

--- mutate.c
+++ mutate.c
@@ -2,102 +2,218 @@
 
 /* Removed as can't shift a void pointer since unknown size, ask Sean
 void
-shift_val(void* in_val)
+shift_val(void* in_val, int *round)
 {
 	int shifter = rand() % (4 + 1 - 0) + 0;
 	(*in_val) << shifter;
 }
 */
 
+#ifndef ROUND_NUM
+#define ROUND_NUM 1
+#endif // ROUND_NUM
+
 void
-mut_int(int* in_val)
+mut_int(int* in_val, int *round)
 {
-	(*in_val) << (rand() % (4 + 1 - 0) + 0);
-	(*in_val) |= (rand() % (15 + 1 - 0) + 0);
+    if(*round == ROUND_NUM)
+    {
+        *in_val = rand();
+    }
+    else
+    {
+        (*in_val) << (rand() % (4 + 1 - 0) + 0);
+        (*in_val) |= (rand() % (15 + 1 - 0) + 0);
+    }
 }
 
 void
-mut_uint(unsigned int* in_val)
+mut_uint(unsigned int* in_val, int *round)
 {
-	(*in_val) << (rand() % (4 + 1 - 0) + 0);
-	(*in_val) |= (rand() % (15 + 1 - 0) + 0);
+    if(*round == ROUND_NUM)
+    {
+        *in_val = rand();
+    }
+    else
+    {
+        (*in_val) << (rand() % (4 + 1 - 0) + 0);
+        (*in_val) |= (rand() % (15 + 1 - 0) + 0);
+    }
 }
 
 void
-mut_charstar(char** in_val)
+mut_charstar(char** in_val, int *round)
 {
+    if(*round == ROUND_NUM)
+    {
+        //*in_val = rand();
+    }
+    else
+    {
+
+    }
 }
 
 void
-mut_ucharstar(unsigned char** in_val)
+mut_ucharstar(unsigned char** in_val, int *round)
 {
+    if(*round == ROUND_NUM)
+    {
+        //*in_val = rand();
+    }
+    else
+    {
+    }
 }
 
 /*
 void
-mut_charstar_arr(char*[]* in_val)
+mut_charstar_arr(char*[]* in_val, int *round)
 {
+    if(*round == ROUND_NUM)
+    {
+        *in_val = rand();
+    }
+    else
+    {
 	int shifter = rand() % (4 + 1 - 0) + 0;
 	(*in_val) << shifter;
 }*/
 
 void
-mut_long(long* in_val)
+mut_long(long* in_val, int *round)
 {
-	(*in_val) << (rand() % (4 + 1 - 0) + 0);
-	(*in_val) |= (rand() % (15 + 1 - 0) + 0);
+    if(*round == ROUND_NUM)
+    {
+        //*in_val = rand();
+    }
+    else
+    {
+        (*in_val) << (rand() % (4 + 1 - 0) + 0);
+        (*in_val) |= (rand() % (15 + 1 - 0) + 0);
+    }
 }
 
 void
-mut_longstar(long** in_val)
+mut_longstar(long** in_val, int *round)
 {
+    if(*round == ROUND_NUM)
+    {
+        //*in_val = rand();
+    }
+    else
+    {
+    }
 }
 
 void
-mut_ulong(unsigned long* in_val)
+mut_ulong(unsigned long* in_val, int *round)
 {
-	(*in_val) << (rand() % (4 + 1 - 0) + 0);
-	(*in_val) |= (rand() % (15 + 1 - 0) + 0);
+    if(*round == ROUND_NUM)
+    {
+        //*in_val = rand();
+    }
+    else
+    {
+        (*in_val) << (rand() % (4 + 1 - 0) + 0);
+        (*in_val) |= (rand() % (15 + 1 - 0) + 0);
+    }
 }
 
 void
-mut_vlong(long long* in_val)
+mut_vlong(long long* in_val, int *round)
 {
-	(*in_val) << (rand() % (4 + 1 - 0) + 0);
-	(*in_val) |= (rand() % (15 + 1 - 0) + 0);
+    if(*round == ROUND_NUM)
+    {
+        //*in_val = rand();
+    }
+    else
+    {
+        (*in_val) << (rand() % (4 + 1 - 0) + 0);
+        (*in_val) |= (rand() % (15 + 1 - 0) + 0);
+    }
 }
 
 void
-mut_void(void* in_val)
+mut_void(void* in_val, int *round)
 {
+    if(*round == ROUND_NUM)
+    {
+        //*in_val = rand();
+    }
+    else
+    {
+    }
 }
 
 void
-mut_voidstar(void** in_val)
+mut_voidstar(void** in_val, int *round)
 {
+    if(*round == ROUND_NUM)
+    {
+        //*in_val = rand();
+    }
+    else
+    {
+    }
 }
 
 void
-mut_IOchunk(IOchunk* in_val)
-{	
+mut_IOchunk(IOchunk* in_val, int *round)
+{
+    if(*round == ROUND_NUM)
+    {
+        //*in_val = rand();
+    }
+    else
+    {
+    }
 }
 
 void
-mut_IOchunkstar(IOchunk** in_val)
+mut_IOchunkstar(IOchunk** in_val, int *round)
 {
+    if(*round == ROUND_NUM)
+    {
+        //*in_val = rand();
+    }
+    else
+    {
+    }
 }
 
 void
-mut_dir(Dir* in_val)
+mut_dir(Dir* in_val, int *round)
 {
+    if(*round == ROUND_NUM)
+    {
+        //*in_val = rand();
+    }
+    else
+    {
+    }
 }
 
 void
-mut_dirstar(Dir** in_val)
+mut_dirstar(Dir** in_val, int *round)
 {
+    if(*round == ROUND_NUM)
+    {
+        //*in_val = rand();
+    }
+    else
+    {
+    }
 }
 
 void
-mut_dirstar_star(Dir*** in_val)
+mut_dirstar_star(Dir*** in_val, int *round)
 {
+    if(*round == ROUND_NUM)
+    {
+        //*in_val = rand();
+    }
+    else
+    {
+    }
 }
