be56c70d35462c0ced615b81187689ea8829201c
Steven Sleder <ssleder@iastate.edu>
2018-11-24 05:07:58 -0600

    Added in mutations for long* and int*

---
+++ main.c
@@ -0,0 +1,129 @@
+#include "fuzz.h"
+
+// Global variables are bad
+int		logfd = -1; // fd of the log file, initialized in main
+Lock	loglck;		// Lock for logger
+
+// Commandline usage warning
+void
+usage(void)
+{
+	fprint(2, "usage: %s [-n rounds] calls\n", argv0);
+	exits("usage");
+}
+
+// Perform locked logging operation -- wraps print
+void
+dolog(char *fmt, ...)
+{
+	va_list args;
+	va_start(args, fmt);
+
+	lock(&loglck);
+	vfprint(logfd, fmt, args);
+	unlock(&loglck);
+
+	va_end(args);
+}
+
+/* Prototypes */
+void	initsctable(void);
+int		name2index(char*);
+
+
+// A kernel fuzzer for the Plan 9 kernel
+void
+main(int argc, char *argv[])
+{
+	int nrounds = -1, i;
+	List tofuzz = mklist() ; // List of syscall table ID's to fuzz
+
+	ARGBEGIN{
+		case 'n':
+			// Number of rounds to iterate fuzzing for
+			nrounds = atoi(ARGF());
+			break;
+		default:
+			usage();
+	}ARGEND
+
+	// Initialize the table of all system calls
+	initsctable();
+
+	// Acquire a list of calls specified by spaces (fuzz -n 1 read write seek)
+	for(;*argv;argv++){
+		int index;
+		if((index = name2index(*argv)) > 0){
+			#ifdef DEBUG 
+			print("DEBUG index: %d\n", index);
+			#endif
+			dolog("Loading call: %s\n", *argv);
+			ladd(&tofuzz, &syscalls[index]); // Might be dangerous, pls fix
+		}else{
+			print("Error: Invalid system call: %s\n", *argv);
+			exits("Encountered invalid syscall");
+		}
+	}
+	
+	logfd = create("./fuzz.log", OWRITE, 0777);
+	if(logfd < 0){
+		fprint(2, "Error: Failed to create/open log file.");
+		exits("log file create fail");
+	}
+
+	// save so we don't have two different time(0)'s
+	int fuzz_seed = time(0);
+	srand(fuzz_seed);
+	dolog("==Seed Value: %d==\n", fuzz_seed);
+
+	// Operate for the desired number of rounds, -1 is infinite
+	for(i = 0; i < nrounds || nrounds < 0; i++){
+		int j;
+		dolog("== Begin round %d ==\n", i);
+		for(j = 0; j < tofuzz.size; j++){
+			caller *fcall = (caller*) lget(&tofuzz, j);
+			dolog("­­ Fuzzing: %s ­­\n", fcall->name);
+			
+			// Someone in here is calling exits inappropriately so forking.
+			int pid = rfork(RFFDG|RFREND|RFPROC|RFMEM);
+			if(pid == 0){
+				// Child
+				fuzz(fcall); // Fuzz, syncs the disk
+				exits(nil);
+			}
+		}
+	}
+
+	close(logfd);
+	exits(nil);
+}
+
+
+// Initialize the syscall table -- hopefully deprecated by a lex/yacc builder
+void
+initsctable(void)
+{
+	int i;
+	for(i = 0; i < NCALLS; i++){
+		syscalls[i].c = (call)i;
+		syscalls[i].name = callnames[i]; // Pointer points to callnames
+		syscalls[i].round = -1;
+		syscalls[i].seed = -1;
+		mkinlist(&(syscalls[i].inputs), (call)i);
+	}
+}
+
+// Given a syscall name, return the index it occurs -- or -1 if not found
+int
+name2index(char* name)
+{
+	int i;
+	for(i = 0; i < NCALLS; i++){
+		#ifdef DEBUG
+		print("DEBUG cmp %s to %s on %d\n", syscalls[i].name, name, i);
+		#endif
+		if(strcmp(syscalls[i].name, name) == 0)
+			return i;
+	}
+	return -1;
+}
---
+++ mutate.c
@@ -0,0 +1,233 @@
+#include "fuzz.h"
+
+#ifndef ROUND_NUM
+#define ROUND_NUM 1
+#endif // ROUND_NUM
+
+void
+mut_int(int* in_val, int *round)
+{
+    if(*round == ROUND_NUM)
+    {
+	dolog("First seeding of rand\n");
+        *in_val = rand();
+    }
+    else
+    {
+        (*in_val) << (rand() % (4 + 1 - 0) + 0);
+        (*in_val) |= (rand() % (15 + 1 - 0) + 0);
+    }
+}
+
+void
+mut_intstar(int** in_val, int* round)
+{
+	// TODO -- Sean sanity check plz
+	if(*round == ROUND_NUM)
+	{
+		in_val = (int**) malloc(sizeof(int*));
+		*in_val = (int*) malloc(sizeof(int));
+		**in_val = rand();
+	}
+	else
+	{
+	        (**in_val) << (rand() % (4 + 1 - 0) + 0);
+        	(**in_val) |= (rand() % (15 + 1 - 0) + 0);
+	}
+}
+
+void
+mut_uint(unsigned int* in_val, int *round)
+{
+    if(*round == ROUND_NUM)
+    {
+        *in_val = rand();
+    }
+    else
+    {
+        (*in_val) << (rand() % (4 + 1 - 0) + 0);
+        (*in_val) |= (rand() % (15 + 1 - 0) + 0);
+    }
+}
+
+void
+mut_charstar(char** in_val, int *round)
+{
+    if(*round == ROUND_NUM)
+    {
+        //*in_val = rand();
+    }
+    else
+    {
+
+    }
+}
+
+void
+mut_ucharstar(unsigned char** in_val, int *round)
+{
+    if(*round == ROUND_NUM)
+    {
+        //*in_val = rand();
+    }
+    else
+    {
+    }
+}
+
+/*
+void
+mut_charstar_arr(char*[]* in_val, int *round)
+{
+    if(*round == ROUND_NUM)
+    {
+        *in_val = rand();
+    }
+    else
+    {
+	int shifter = rand() % (4 + 1 - 0) + 0;
+	(*in_val) << shifter;
+}*/
+
+void
+mut_long(long* in_val, int *round)
+{
+    if(*round == ROUND_NUM)
+    {
+        *in_val = (rand() << 16) | rand();
+    }
+    else
+    {
+        (*in_val) << (rand() % (4 + 1 - 0) + 0);
+        (*in_val) |= (rand() % (15 + 1 - 0) + 0);
+    }
+}
+
+void
+mut_longstar(long** in_val, int *round)
+{
+    if(*round == ROUND_NUM)
+    {
+	// TODO -- check that this is correct
+	in_val = (long**) malloc(sizeof(long*));
+	*in_val = (long*) malloc(sizeof(long));
+	**in_val = (rand() << 16) | rand();
+    }
+    else
+    {
+	(**in_val) << (rand() % (4 + 1 - 0) + 0);
+	(**in_val) |= (rand() % (15 + 1 - 0 ) + 0);
+    }
+}
+
+void
+mut_ulong(unsigned long* in_val, int *round)
+{
+    if(*round == ROUND_NUM)
+    {
+        *in_val = (rand() << 16) | rand();
+    }
+    else
+    {
+        (*in_val) << (rand() % (4 + 1 - 0) + 0);
+        (*in_val) |= (rand() % (15 + 1 - 0) + 0);
+    }
+}
+
+void
+mut_vlong(long long* in_val, int *round)
+{
+    if(*round == ROUND_NUM)
+    {
+        *in_val = (rand() << 48) | (rand() << 32) | (rand() << 16) | rand();
+    }
+    else
+    {
+        (*in_val) << (rand() % (4 + 1 - 0) + 0);
+        (*in_val) |= (rand() % (15 + 1 - 0) + 0);
+    }
+}
+
+void
+mut_void(void* in_val, int *round)
+{
+    if(*round == ROUND_NUM)
+    {
+        //*in_val = rand();
+    }
+    else
+    {
+    }
+}
+
+void
+mut_voidstar(void** in_val, int *round)
+{
+    if(*round == ROUND_NUM)
+    {
+        //*in_val = rand();
+    }
+    else
+    {
+    }
+}
+
+void
+mut_IOchunk(IOchunk* in_val, int *round)
+{
+    if(*round == ROUND_NUM)
+    {
+        //*in_val = rand();
+    }
+    else
+    {
+    }
+}
+
+void
+mut_IOchunkstar(IOchunk** in_val, int *round)
+{
+    if(*round == ROUND_NUM)
+    {
+        //*in_val = rand();
+    }
+    else
+    {
+    }
+}
+
+void
+mut_dir(Dir* in_val, int *round)
+{
+    if(*round == ROUND_NUM)
+    {
+        //*in_val = rand();
+    }
+    else
+    {
+    }
+}
+
+void
+mut_dirstar(Dir** in_val, int *round)
+{
+    if(*round == ROUND_NUM)
+    {
+        //*in_val = rand();
+    }
+    else
+    {
+    }
+}
+
+void
+mut_dirstar_star(Dir*** in_val, int *round)
+{
+    if(*round == ROUND_NUM)
+    {
+        //*in_val = rand();
+    }
+    else
+    {
+    }
+}
